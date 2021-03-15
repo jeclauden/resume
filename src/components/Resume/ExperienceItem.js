03d96fae738899cfb93cb823785e4da07871cd7a
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2021-03-15 16:26:17 +1030

    Update ExperienceItem.js

--- src/components/Resume/ExperienceItem.js
+++ src/components/Resume/ExperienceItem.js
@@ -2,12 +2,14 @@ import React from "react";
 import { ColumnTwo, MainContainer, SubTitle } from "./MainElements";
 
 const ExperienceItem = ({ subtitle, experience }) => {
-  const content = experience.map((object) => <li key={object}>{object}</li>);
+  const content = <ul style={{ marginLeft: "40px" }}> 
+  {experience.map((object) => 
+  <li key={object}>{object}</li>)} </ul>
   return (
     <MainContainer>
       <ColumnTwo style={{ width: "100%", paddingLeft: "0px" }}>
         <SubTitle>{subtitle}</SubTitle>
-        <ul style={{ marginLeft: "40px" }}> {content} </ul>
+        {content}
       </ColumnTwo>
     </MainContainer>
   );

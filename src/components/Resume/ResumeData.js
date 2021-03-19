6e48502d85d64ff50597c081554ef380716abd92
Jean-Claude Ndagijimana <jeclauded@gmail.com>
2021-03-19 23:45:33 +1030

    Update ResumeData.js

--- src/components/Resume/ResumeData.js
+++ src/components/Resume/ResumeData.js
@@ -26,14 +26,13 @@ export const EmploymentObjects = [
     dates: "Jul 2020 - Nov 2020",
     jobRole: "Intern Web Developer",
     experience: [
-      "Developed an interactive dashboard (REAL TIME CHANGE AND TEST DATA DASHBOARD) to dynamically display projects and Request for Change (RFCs) and associated test data information.",
+      "Developed a web-based interactive dashboard (Real Time Change and Test Data Dashboard) to dynamically display projects and Request for Change (RFCs) and associated test data information.",
       "Learned to use AdobeXD on spot.",
       "Designed and continuously updated the dashboard according to the stakeholders’ feedbacks.",
       "Documented and communicated design decisions and project challenges with stakeholders.",
       "Utilised: AdobeXD, HTML, CSS, SASS, JavaScript, Bootstrap and Git.",
     ],
-  }
-
+  },
 ];
 
 export const ExperienceObjects = [
@@ -50,7 +49,7 @@ export const ExperienceObjects = [
     experience: [
       "Developed a text-based adventure game to play by taking turns and selecting actions based on pre-configured keyboard inputs.",
       "Worked on analysis, design and creation of UML class diagram and applied appropriate design patterns",
-      "Utilised: C++, Qt Creator and Git",
+      "Utilised: C++, Qt Creator and Git.",
     ],
   },
   {
@@ -58,7 +57,7 @@ export const ExperienceObjects = [
     experience: [
       "Developed an interface application that allows a client to make and reschedule appointments.",
       "Created personas, user stories, use cases, prototypes and documented design decisions.",
-      "Utilised: Eclipse, JavaFX and Git",
+      "Utilised: Eclipse, JavaFX and Git.",
     ],
   },
   {
@@ -67,7 +66,7 @@ export const ExperienceObjects = [
       "Created a simple website that loads South Australia Police expiation data by using jQuery (AJAX request)",
       "Created a table dynamically with interactive rows using Bootstrap, CSS and DOM JavaScript",
       "Used D3 JavaScript library to present data visually and dynamically on bar graphs.",
-      "Utilised: Visual Studio, D3, HTML, Bootstrap, CSS, jQuery and Git",
+      "Utilised: Visual Studio, D3, HTML, Bootstrap, CSS, jQuery and Git.",
     ],
   },
   {
@@ -75,7 +74,7 @@ export const ExperienceObjects = [
     experience: [
       "Developed a mock website that lists company products for sale. The website enables a user to see product details and other related information.",
       "Used Microsoft ASP.NET MVC architecture and ASP.NET API to structure and populate web pages.",
-      "Utilised: Visual Studio, HTML, Bootstrap, CSS, jQuery, JavaScript and Git",
+      "Utilised: Visual Studio, HTML, Bootstrap, CSS, jQuery, JavaScript and Git.",
     ],
   },
 ];
